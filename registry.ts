2c2df185f4cb9e38c02e39cc04488eac44d0f393
aaort <artashesano@gmail.com>
2025-06-14 11:23:45 +0400

    attempt to setup proper registry structure

--- registry.ts
+++ registry.ts
@@ -1,27 +1,27 @@
 import { RegistryEntry } from "./scripts/schema";
 
 const registry: RegistryEntry[] = [
-  {
-    name: "cool-text",
-    type: "registry:ui",
+	{
+		name: "ghibli-theme",
+		type: "registry:ui",
 
-    // shadcn-ui components that this component depends on
-    registryDependencies: ["button"],
+		// shadcn-ui components that this component depends on
+		registryDependencies: ["button"],
 
-    // npm dependencies that this component depends on
-    dependencies: [],
-    devDependencies: [],
+		// npm dependencies that this component depends on
+		dependencies: [],
+		devDependencies: [],
 
-    // Tailwind CSS config
-    tailwind: {
-      config: {},
-    },
+		// Tailwind CSS config
+		tailwind: {
+			config: {},
+		},
 
-    // CSS variables
-    cssVars: {},
+		// CSS variables
+		cssVars: {},
 
-    // Files that make up this component in your src/components/ui folder
-    files: ["cool-text.tsx"],
-  },
+		// Files that make up this component in your src/components/ui folder
+		files: ["button.tsx"],
+	},
 ];
 export default registry;
