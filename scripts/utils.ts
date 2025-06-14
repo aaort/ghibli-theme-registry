2eb273ffafcac7b75d2be505c228c2c0a7423713
aaort <artashesano@gmail.com>
2025-06-14 13:37:01 +0400

    format properly the entire project js/ts/json code

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -3,24 +3,24 @@ import parseGitHubUrl from "parse-github-url";
 import { RegistryEntry } from "./schema";
 
 export async function getGitHubBaseUrl() {
-  const packageJson = JSON.parse(await readFile("./package.json", "utf-8"));
+	const packageJson = JSON.parse(await readFile("./package.json", "utf-8"));
 
-  if (!packageJson.repository) {
-    return "https://example.com/registry";
-  }
+	if (!packageJson.repository) {
+		return "https://example.com/registry";
+	}
 
-  const url = parseGitHubUrl(packageJson.repository.url);
-  if (!url) {
-    return "https://example.com/registry";
-  }
+	const url = parseGitHubUrl(packageJson.repository.url);
+	if (!url) {
+		return "https://example.com/registry";
+	}
 
-  return `https://raw.githubusercontent.com/${url.repo}/main/registry`;
+	return `https://raw.githubusercontent.com/${url.repo}/main/registry`;
 }
 
 export function getComponentBasePath(type: RegistryEntry["type"]) {
-  if (type === "registry:component") {
-    return "./src/components";
-  }
+	if (type === "registry:component") {
+		return "./src/components";
+	}
 
-  return "./src/components/ui";
+	return "./src/components/ui";
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/shadcn-registry-template",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react()],
+	base: "/shadcn-registry-template",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
