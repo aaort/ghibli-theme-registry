7863a52a8f92c5ab03d02d90d700012dd9ea052f
aaort <artashesano@gmail.com>
2025-06-14 14:19:50 +0400

    remove `components/ui/cool-text.tsx`

--- src/components/ui/cool-text.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function CoolText({ children }: { children: React.ReactNode }) {
-	// @ts-expect-error Marquee is deprecated
-	return <marquee className="text-secondary">{children}</marquee>;
-}
-
-export default CoolText;
