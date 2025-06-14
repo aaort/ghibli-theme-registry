99a3d50bdaf51abcdc9b308b9882daacbc3bd685
aaort <artashesano@gmail.com>
2025-06-14 15:13:48 +0400

    use proper imports for latest version of tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Button } from "./components/ui/button";
 
 function App() {
-	return (
-		<div className="max-w-xl mx-auto my-12 rounded-lg border border-gray-200 p-8">
-			<h1 className="text-2xl font-bold text-gray-900 mb-4">Custom Registry</h1>
-			<p>Add your custom components here to preview them.</p>
+  return (
+    <div className="max-w-xl mx-auto my-12 rounded-lg border border-gray-200 p-8">
+      <h1 className="text-2xl font-bold text-gray-900 mb-4">Custom Registry</h1>
+      <p>Add your custom components here to preview them.</p>
 
-			<Button>Click me </Button>
-		</div>
-	);
+      <Button variant="secondary" className="bg-red-500">
+        Click me{" "}
+      </Button>
+    </div>
+  );
 }
 
 export default App;
